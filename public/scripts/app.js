14705e1f95497156e72874133eae19414727c99e
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-11-29 15:34:22 -0800

    finished sprint 3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,7 +17,7 @@ function AlbumsIndexController ($http) {
     name: 'Viva Hate',
     artistName: 'Morrissey'
   };
-
+//Get Read albums
   $http({
     method: 'GET',
     url: '/api/albums'
@@ -26,7 +26,7 @@ function AlbumsIndexController ($http) {
   }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
   });
-
+//Create Album
   vm.createAlbum = function () {
     $http({
       method: 'POST',
@@ -38,4 +38,34 @@ function AlbumsIndexController ($http) {
       console.log('There was an error posting the data', response);
     });
   }
+
+
+
+  vm.editAlbum = function (album) {
+    $http({
+      method: 'PUT',
+      url: '/api/albums/'+album._id,
+      data: album,
+    }).then(function successCallback(response) {
+      console.log('hey congrats you upadted an item', response)
+    }, function errorCallback(response) {
+      console.log('There was an error editing the data', response);
+    });
+  }
+
+
+
+//Delete Album
+  vm.deleteAlbum = function (album) {
+  $http({
+    method: 'DELETE',
+    url: '/api/albums/'+ album._id
+  }).then(function successCallback(deletedAlbum) {
+    var index = vm.albums.indexOf(deletedAlbum);
+    vm.albums.splice(index, 1);
+  }, function errorCallback(response) {
+    console.log('There was an error deleting the data', response);
+  });
+}
+
 }
